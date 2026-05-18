a828b704f29993fca3b5ebdca7f5db9a7f002da9
SivaHariharan202 <sivahariharan202@gmail.com>
2026-05-19 00:18:26 +0530

    remove for deployment setting

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -1,60 +1,60 @@
-import os
-import dj_database_url
-from .settings import *
-from .settings import BASE_DIR
+# import os
+# import dj_database_url
+# from .settings import *
+# from .settings import BASE_DIR
 
-ALLOWED_HOSTS == [os.environ.get['RENDER_EXTERNAL_HOST_NAME']]
-CSRF_TRUSTED_ORIGINS =['https://'+os.environ.get['RENDER_EXTERNAL_HOST_NAME']]
+# ALLOWED_HOSTS == [os.environ.get['RENDER_EXTERNAL_HOST_NAME']]
+# CSRF_TRUSTED_ORIGINS =['https://'+os.environ.get['RENDER_EXTERNAL_HOST_NAME']]
 
-DEBUG=False
+# DEBUG=False
 
-SECRET_KEY= os.environ.get['SECRET_KEY']
+# SECRET_KEY= os.environ.get['SECRET_KEY']
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware', 
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+# MIDDLEWARE = [
+#     'corsheaders.middleware.CorsMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'whitenoise.middleware.whiteNoiseMiddleware', 
+#     "django.middleware.security.SecurityMiddleware",
+#     "django.contrib.sessions.middleware.SessionMiddleware",
+#     "django.middleware.common.CommonMiddleware",
+#     "django.middleware.csrf.CsrfViewMiddleware",
+#     "django.contrib.auth.middleware.AuthenticationMiddleware",
+#     "django.contrib.messages.middleware.MessageMiddleware",
+#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+# ]
 
-CORS_ALLOWD_ORIGINS = ['http://localhost:8000']
-
-
-STORAGE ={
-    "defaults":{
-        "BACKEND":"django.core.files.storage.FileSystemStorage"
-    },
-    "staticfiles":{
-        "BACKEND":"whitenoise.storage.CompressedStaticFilesStorage"
-    }
-}
+# CORS_ALLOWD_ORIGINS = ['http://localhost:8000']
 
 
-
-# DATABASES = {
-#     'default':dj_database_url.config(
-#             default =os.environ['DATABASE_URL'],
-#             conn_max_age
-#     )
-# }
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'test_db',
-#         'USER': 'postgres',        # or fabric_user
-#         'PASSWORD': 'Sivahari@123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+# STORAGE ={
+#     "defaults":{
+#         "BACKEND":"django.core.files.storage.FileSystemStorage"
+#     },
+#     "staticfiles":{
+#         "BACKEND":"whitenoise.storage.CompressedStaticFilesStorage"
 #     }
 # }
+
+
+
+# # DATABASES = {
+# #     'default':dj_database_url.config(
+# #             default =os.environ['DATABASE_URL'],
+# #             conn_max_age
+# #     )
+# # }
+
+
+
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.postgresql',
+# #         'NAME': 'test_db',
+# #         'USER': 'postgres',        # or fabric_user
+# #         'PASSWORD': 'Sivahari@123',
+# #         'HOST': 'localhost',
+# #         'PORT': '5432',
+# #     }
+# # }
 
 
