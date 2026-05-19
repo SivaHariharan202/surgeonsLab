fc79bdd56f7da1a22ca6e7dc1b9c8e441a971228
SivaHariharan202 <sivahariharan202@gmail.com>
2026-05-19 10:42:59 +0530

    added for correct host in backend

--- frontend/src/config/ApiHost.js
+++ frontend/src/config/ApiHost.js
@@ -1,5 +1,6 @@
 
-const DEFAULT_HOST = "http://localhost:8000/api/";
+// const DEFAULT_HOST = "http://localhost:8000/api/";
+const DEFAULT_HOST = "https://surgeonslab-backend.onrender.com/api/";
 
 
 export const getApiHost = () => {
