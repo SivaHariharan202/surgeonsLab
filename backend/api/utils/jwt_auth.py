1b0fcc0f95c305287f8215c5a6ceaa0cf806f85c
SivaHariharan202 <sivahariharan202@gmail.com>
2026-05-19 11:33:23 +0530

    jwt update

--- backend/api/utils/jwt_auth.py
+++ backend/api/utils/jwt_auth.py
@@ -1,3 +1,35 @@
+# from rest_framework_simplejwt.tokens import AccessToken
+# from ..model.model import TaskAppUser
+
+
+# def get_logged_user(request):
+
+#     try:
+#         auth_header = request.headers.get(
+#             'Authorization'
+#         )
+
+#         if not auth_header:
+#             return None
+
+#         token = auth_header.split(' ')[1]
+
+#         decoded_token = AccessToken(token)
+
+#         user_id = decoded_token.get(
+#             'user_id'
+#         )
+
+#         user = TaskAppUser.objects.get(
+#             id=user_id
+#         )
+
+#         return user
+
+#     except Exception as e:
+#         print("JWT ERROR:", str(e))
+#         return None
+
 from rest_framework_simplejwt.tokens import AccessToken
 from ..model.model import TaskAppUser
 
@@ -9,20 +41,29 @@ def get_logged_user(request):
             'Authorization'
         )
 
+        print("AUTH HEADER:", auth_header)
+
         if not auth_header:
+            print("NO AUTH HEADER")
             return None
 
         token = auth_header.split(' ')[1]
 
         decoded_token = AccessToken(token)
 
+        print("DECODED TOKEN:", decoded_token)
+
         user_id = decoded_token.get(
             'user_id'
         )
 
-        user = TaskAppUser.objects.get(
+        print("USER ID:", user_id)
+
+        user = TaskAppUser.objects.filter(
             id=user_id
-        )
+        ).first()
+
+        print("USER:", user)
 
         return user
 
